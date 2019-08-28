a12f3a6d131bc95d13ecd986340ae953a3cd3dc1
Artur Fatkulin <38537124+arturfatkul@users.noreply.github.com>
2019-08-29 00:15:09 +0500

    Update password_generator.sh

--- password_generator.sh
+++ password_generator.sh
@@ -1,19 +1,43 @@
 #!/bin/bash
 
+usage() {
+    echo "Usage: ${0} [-l LENGTH]" >&2
+    echo "Generate a random password."
+    echo "  -l LENGTH  Specify the password length (multiple of four)"
+}
+
+if [[ ${#} -eq 0 ]]
+then
+    usage
+fi
+
 LOW='qwertyuiopasdfghjklzxcvbnm'
 CAP='QWERTYUIOPASDFGHJKLZXCVBNM'
 SPECIAL='!@#$%^&*()_+{}[];:?></\|-='
 INTEGER='1234567890'
+LENGTH=0
 
-# You can change the length of the password by changing the interger value inside UNTIL loop condition
-until [[ x -gt 5 ]]
+while getopts l: OPTION
+do
+  case ${OPTION} in
+    l)
+      LENGTH=${OPTARG}
+      ;;
+    ?)
+      usage
+      ;;
+   esac
+done
+
+
+until [[ x -ge ${LENGTH}  ]]
 do 
     ((x=x+1))
-    # Get random character
+    # Get random number from the variables
     CHAR1=$(echo ${LOW} | fold -w1 | shuf | head -c1)
     CHAR2=$(echo ${CAP} | fold -w1 | shuf | head -c1)
     CHAR3=$(echo ${SPECIAL} | fold -w1 | shuf | head -c1)
-    CHAR4=$(echo ${INTEGER} | fold -w1 | shuf | head -c1)
+    CHAR4=$(echo ${INTEGER} | fold -w1 | shuf | head -c1) 
     ALL_CHARS=${CHAR1}${CHAR2}${CHAR3}${CHAR4}
     ARRAY+=${ALL_CHARS}
     # Randomization to change the order 
