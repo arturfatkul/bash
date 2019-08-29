d624a95c4d6d544758320275502fc63a688fa06e
Artur Fatkulin <38537124+arturfatkul@users.noreply.github.com>
2019-08-29 13:24:12 +0500

    Update password_generator.sh

--- password_generator.sh
+++ password_generator.sh
@@ -3,7 +3,7 @@
 usage() {
     echo "Usage: ${0} [-l LENGTH]" >&2
     echo "Generate a random password."
-    echo "  -l LENGTH  Specify the password length (multiple of four)"
+    echo "  -l LENGTH  Specify the password length"
 }
 
 if [[ ${#} -eq 0 ]]
@@ -24,20 +24,26 @@ do
    esac
 done
 
-
 until [[ x -ge ${LENGTH}  ]]
-do 
-    ((x=x+1))
+do
     # Get random number from the variables
     CHAR1=$(echo ${LOW} | fold -w1 | shuf | head -c1)
     CHAR2=$(echo ${CAP} | fold -w1 | shuf | head -c1)
     CHAR3=$(echo ${SPECIAL} | fold -w1 | shuf | head -c1)
     CHAR4=$(echo ${INTEGER} | fold -w1 | shuf | head -c1) 
-    ALL_CHARS=${CHAR1}${CHAR2}${CHAR3}${CHAR4}
-    ARRAY+=${ALL_CHARS}
-    # Randomization to change the order 
-    RANDOM_PASSWORD=$(echo ${ARRAY} | fold -w1 | shuf | tr -d '\n')
+
+    for i in ${CHAR1} ${CHAR2} ${CHAR3} ${CHAR4}
+    do
+        ((x=x+1))
+        if [[ x -le ${LENGTH} ]]
+        then
+            ARRAY+=${i}
+        fi
+    done
 done
+    
+# Randomization to change the order 
+RANDOM_PASSWORD=$(echo ${ARRAY} | fold -w1 | shuf | tr -d '\n')
 
 echo "${RANDOM_PASSWORD}"
 
